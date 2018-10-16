9e3779d235f1d55e8a2c51b47a667fd404c9e0e1
Soumik Mandal <32775854+soumik1998@users.noreply.github.com>
2018-10-17 01:08:33 +0530

    URL is Good.

--- IIITSKART/cart/urls.py
+++ IIITSKART/cart/urls.py
@@ -22,7 +22,6 @@ urlpatterns = [
     url(r'^',include(router.urls)),
     path('accounts/', include('django.contrib.auth.urls')),
     path('login-view/', views.login_page , name='login_page'),
-<<<<<<< HEAD
     path(r'/profilevalidation/',views.profile_val , name = 'profile_val'),
     path(r'/profilevalidationapi/', views.profile_val_api, name='profile_val_api'),
     path(r'/makeuser/', views.makeuser, name='makeuser'),
@@ -31,17 +30,7 @@ urlpatterns = [
     path(r'/go-to-dashboard/', views.go_to_dashboard, name='go-to-dashboard'),
     path(r'/search/', views.search, name='search'),
     path(r'/logout/', views.logout_view , name='logout'),
-=======
-    path('profilevalidation/',views.profile_val , name = 'profile_val'),
-    path('profilevalidationapi/', views.profile_val_api, name='profile_val_api'),
-    path('makeuser/', views.makeuser, name='makeuser'),
-    path(r'receive/', views.receive, name='receive'),
-    path(r'send/', views.send, name='send'),
-    path(r'go-to-dashboard/', views.go_to_dashboard, name='go-to-dashboard'),
-    path(r'search/', views.search, name='search'),
-    path(r'profile/', views.profile_view, name='profile'),
-    path(r'logout/', views.logout_view , name='logout'),
->>>>>>> 4cf62b67769547a15c6a59b057184c9faafcf1de
+    path(r'/profile/', views.profile_view, name='profile'),
 
 
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
